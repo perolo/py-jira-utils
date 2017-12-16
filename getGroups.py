48187e3286b840a96b68916db190eb679889d10c
Per Olofsson <per.e.olofsson@gmail.com>
2017-12-16 23:12:19 +0100

    PEP8 Cleanup

--- getGroups.py
+++ getGroups.py
@@ -2,31 +2,29 @@ from jira import JIRA
 import pickle
 
 
-
 def main():
-    errCount = 0
     i = 0
-    jira = JIRA("http://localhost:8080",basic_auth=('pro', 'pro'))
+    jira = JIRA("http://localhost:8080", basic_auth=('pro', 'pro'))
 
     groups = jira.groups(maxResults=10)
 
-    myArray = []
+    my_array = []
 
     for group in groups:
         j = 0
-        myArray.append([group])
+        my_array.append([group])
         print("Group Name: " + group)
         j = j+1
         members = jira.group_members(group)
 
         for member in members:
-            myArray[i].append(member)
+            my_array[i].append(member)
             print("     Member: " + member)
             j = j + 1
-        i=i+1
+        i = i + 1
 
-    pickle.dump( myArray, open( "groupBackup.p", "wb" ) )
+    pickle.dump(my_array, open("groupBackup.p", "wb"))
+
 
 if __name__ == '__main__':
     main()
-
--- setGroups.py
+++ setGroups.py
@@ -2,30 +2,26 @@ from jira import JIRA
 import pickle
 
 
-
 def main():
-    errCount = 0
-    i = 0
-    jira = JIRA("http://localhost:8080",basic_auth=('pro', 'pro'))
+    err_count = 0
+    jira = JIRA("http://localhost:8080", basic_auth=('pro', 'pro'))
 
-    restGroups = pickle.load( open( "groupBackup.p", "rb" ) )
-    for rGroup in restGroups:
-        print("Restore Group Name: " + rGroup[0])
-        jj=0
-        for col in rGroup:
-            if (jj!=0):
-                print("     Name: " + col)
+    rest_groups = pickle.load(open("groupBackup.p", "rb"))
+    for r_group in rest_groups:
+        print("Restore Group Name: "+r_group[0])
+        jj = 0
+        for col in r_group:
+            if jj != 0:
+                print("     Name: "+col)
                 try:
-                    resp = jira.add_user_to_group(col,rGroup[0])
+                    jira.add_user_to_group(col, r_group[0])
                 except Exception as e:
                     if "already a member" not in e.text:
-                        print("     ERR: " + e.text)
-                        errCount = errCount +1
+                        print("     ERR: "+e.text)
+                        err_count = err_count + 1
             jj = jj + 1
-    print("Done - no Errors: " + str(errCount))
-
+    print("Done - no Errors: " + str(err_count))
 
 
 if __name__ == '__main__':
     main()
-
